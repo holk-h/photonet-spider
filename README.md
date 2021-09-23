66a59c072a2cb7d6941dec6218e8972bbb837daa
holk <yellowmac@sina.com>
2021-09-23 14:14:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# photonet-spider
+
+一个[基于photonet的美学评价数据集-DALE](http://infolab.stanford.edu/~wangz/project/imsearch/Aesthetics/ICIP08/datta.pdf?spm=a2c6h.12873639.0.0.77071994LbI7b0&file=datta.pdf )的多线程爬虫
+
+A multiprocessing spider for Algorithmic Inferencing of Aesthetics and Emotion in Natural Images, [DALE](http://infolab.stanford.edu/~wangz/project/imsearch/Aesthetics/ICIP08/datta.pdf?spm=a2c6h.12873639.0.0.77071994LbI7b0&file=datta.pdf)
+
+只需更改 ***spider.py*** 中的 `save_path` 路径名,运行`python3 spider.py`即可
+
+All you need is to change the variable `save_path` in the file ***spider.py***  and run `python3 spider.py`.
+
+#参考论文(Reference)：
+`R. Datta, J. Li, and J. Z. Wang, Algorithmic Inferencing of Aesthetics and Emotion in Natural Images: An Exposition, Proc. IEEE ICIP, Special Session on Image Aesthetics, Mood and Emotion, San Diego, CA, 2008`
---
+++ spider.py
@@ -0,0 +1,55 @@
+import threading
+
+import requests
+import os
+import multiprocessing
+
+save_path = 'C:\\Users\\holk\\IdeaProjects\\photonet-spider\\images\\'
+dataset = 'photonet_dataset.txt'
+f = open(dataset)
+
+
+def download_img(img_url, index):
+    print(img_url)
+    r = requests.get(img_url, stream=True)
+    print(r.status_code)
+    if r.status_code == 200:
+        open(save_path + index + '.jpg', 'wb').write(r.content)
+        print("done")
+    del r
+
+
+def download(begin_index, end_index):
+    for line in f:
+        line = line.strip().split(' ')
+        image_index = line[0]
+
+        if int(image_index) < begin_index:
+            continue
+        elif int(image_index) > end_index:
+            break
+
+        if os.path.isfile(os.path.join(save_path, image_index + '.jpg')):
+            continue
+
+        image_id = line[1]
+        download_img(f"https://www.photo.net/{image_id}-photo.jpg", image_index)
+        print('image%s success' % image_index)
+
+
+if __name__ == '__main__':
+    record = []
+    lock = multiprocessing.Lock()
+
+    index_list = [i for i in range(1, 20278, 1000)]
+    index_list.append(20278)
+
+    for i, j in enumerate(index_list):
+        if i < len(index_list) - 1:
+            print(index_list[i], index_list[i + 1])
+            process = multiprocessing.Process(target=download, args=(index_list[i], index_list[i + 1]))
+            process.start()
+            record.append(process)
+
+    for process in record:
+        process.join()
